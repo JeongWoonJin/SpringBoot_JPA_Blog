429e42c0dc396a548dc9b0237716df61d11f2142
koto1007 <koto89246547@gmail.com>
2021-03-11 14:59:41 +0900

    로그인 기능구현

--- src/main/java/com/jwj/blog/repository/UserRepository.java
+++ src/main/java/com/jwj/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.jwj.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.jwj.blog.model.User;
 
@@ -8,5 +9,10 @@ import com.jwj.blog.model.User;
 //자동으로 Bean으로 등록됨.
 //@Repository 생략가능
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	//JPA Naming 쿼리
+	//SELECT * FROM user WHERE username = ? AND password = ?;
+	User findByUsernameAndPassword(String username, String password);
+	
+//	@Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2;", nativeQuery = true)
+//	User login(String username, String password);
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
 			$("#btn-save").on("click", ()=>{ //function(){}, ()=>{} this를 바인딩 하기위해서 사용함.
 				this.save();
 			});
+			$("#btn-login").on("click", ()=>{ //function(){}, ()=>{} this를 바인딩 하기위해서 사용함.
+				this.login();
+			});
 		},
 		
 		save: function(){
@@ -24,6 +27,28 @@ let index = {
 				dataType: "json"//응답받을 데이터
 			}).done(function(resp){
 				alert("회원가입이 완료되었습니다.");
+				//console.log(resp);
+				location.href="/blog";
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			});
+		},
+		
+		login: function(){
+			//alert("user의 save함수 호출");
+			let data = {
+				username: $("#username").val(),
+				password: $("#password").val()
+			};
+			
+			$.ajax({
+				type: "POST",
+				url: "/blog/api/user/login",
+				data: JSON.stringify(data), //http body 데이터
+				contentType: "application/json; charset=utf-8", //body데이터가 어떤타입인지(MIME)
+				dataType: "json"//응답받을 데이터
+			}).done(function(resp){
+				alert("로그인이 완료되었습니다.");
 				//console.log(resp);
 				location.href="/blog";
 			}).fail(function(error){
